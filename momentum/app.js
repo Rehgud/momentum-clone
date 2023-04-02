7b27b2750f3b7c57685c7fa89333a8ce1788a2f6
Rehgud <meckayryu@gmail.com>
2023-04-02 17:46:33 +0900

    Getting Username

--- momentum/app.js
+++ momentum/app.js
@@ -1,25 +1,21 @@
 const loginForm = document.querySelector("#login-form")
 
-const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
+const loginInput = loginForm.querySelector(`input`);
 const loginButton = loginForm.querySelector("button");
 // const loginInput = document.querySelector("#login-form input");
 // const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
 
 const link = document.querySelector("a");
 
+const HIDDEN_CLASSNAME = "hidden";
 function onLoginSubmit(event) {
-    // console.log(loginInput.value); // loginInput's value
-    // console.log("onLoginBtnClick");
-    /*const username = loginInput.value;
-    if (username === "") { // space username is not input
-        alert("Please write your name.");
-    }else if (username.length > 15){ // username length 제한
-        alert("Your name is too long.")
-    }*/
     event.preventDefault();
-    console.log(loginInput.value); // loginInput's value'
-    // console.log(username);
+    const username = loginInput.value;
+    loginForm.classList.add("HIDDEN_CLASSNAME");
+    // greeting.innerText = "Hello " + username;
+    greeting.innerText = `Hello ${username}`; // ``symbol
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 function handleLinkClick(event) {
